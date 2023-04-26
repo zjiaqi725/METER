0d9c1638f2e5c57c0b83b94e3c25c83ab6d75a9c
zjiaqi725 <57307180+zjiaqi725@users.noreply.github.com>
2023-04-26 17:02:08 +0800

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -16,7 +16,7 @@ def load_dataset(args):
         nfile = 'datasets/kdd.txt'
         lfile = 'datasets/kddlabel.txt'
     elif args.dataset == 'AT':
-        data = pd.read_csv('datasets/numenta/ambient_temperature_system_failure.csv')
+        data = pd.read_csv('datasets/ambient_temperature_system_failure.csv')
         numeric = data["value"].values#.reshape(-1, 1)
         labels = data["label"].values
         X = shingle(numeric, 10) # shape (windowsize, len-win+1)
@@ -24,7 +24,7 @@ def load_dataset(args):
         t1, _ = np.shape(numeric) 
         labels=labels[:t1]
     elif args.dataset == 'CPU':
-        data = pd.read_csv('datasets/numenta/cpu_utilization_asg_misconfiguration.csv')
+        data = pd.read_csv('datasets/cpu_utilization_asg_misconfiguration.csv')
         numeric = data["value"].values
         labels = data["label"].values
         X = shingle(numeric, 10)
@@ -32,7 +32,7 @@ def load_dataset(args):
         t1, _ = np.shape(numeric) 
         labels=labels[:t1]
     elif args.dataset == 'MT':
-        data = pd.read_csv('datasets/numenta/machine_temperature_system_failure.csv')
+        data = pd.read_csv('datasets/machine_temperature_system_failure.csv')
         numeric = data["value"].values
         labels = data["label"].values
         X = shingle(numeric, 10) 
@@ -40,7 +40,7 @@ def load_dataset(args):
         t1, _ = np.shape(numeric) 
         labels=labels[:t1]
     elif args.dataset == 'NYC':
-        data = pd.read_csv('datasets/numenta/nyc_taxi.csv')
+        data = pd.read_csv('datasets/nyc_taxi.csv')
         numeric = data["value"].values
         labels = data["label"].values
         X = shingle(numeric, 10) 
@@ -50,7 +50,7 @@ def load_dataset(args):
     elif args.dataset in ["MNIST_AbrRec", "MNIST_GrdRec", "F_MNIST_AbrRec",
                           "F_MNIST_GrdRec", "GAS", "RIALTO", "INSECTS_Abr", "INSECTS_Incr",
                           "INSECTS_IncrGrd", "INSECTS_IncrRecr"]:
-        data = pd.read_csv("datasets/ARCUS_DATA/"+args.dataset+".csv", dtype=np.float64, header=None)
+        data = pd.read_csv("datasets/"+args.dataset+".csv", dtype=np.float64, header=None)
         data_label = data.pop(data.columns[-1])
         numeric = torch.FloatTensor(data.values)
         labels = data_label.values.reshape(-1)   
